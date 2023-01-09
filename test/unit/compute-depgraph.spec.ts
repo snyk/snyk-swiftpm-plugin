38b332d62074870591234cbe1f864727c3549c47
Doron Eli Rachman <doron.elirachman@snyk.io>
2023-01-09 17:08:59 +0200

    fix: fix tests

--- test/unit/compute-depgraph.spec.ts
+++ test/unit/compute-depgraph.spec.ts
@@ -1,34 +1,56 @@
-import { computeDepGraph } from '../../lib/compute-depgraph';
-import { execute } from '../../lib/subprocess';
-import { dependencies } from '../fixtures/dependencies';
+import {computeDepGraph} from '../../lib/compute-depgraph';
+import {execute} from '../../lib/subprocess';
+import {dependencies} from '../fixtures/dependencies';
 import * as path from "path";
 
 jest.setTimeout(100000);
-const subprocessesMock = jest.mock('../../lib/subprocess');
+jest.mock('../../lib/subprocess');
+
+const mockedExecute = jest.mocked(execute);
+mockedExecute.mockResolvedValue(JSON.stringify(dependencies));
+const SWIFT_DEFAULT_PARAMETERS_COUNT = 6;
 
 describe('compute-depgraph', () => {
-  it('should successfully create snyk dep graph from swift-pm dep tree', async () => {
-    const mockedExecute = jest.mocked(execute);
-    mockedExecute.mockResolvedValueOnce(JSON.stringify(dependencies));
+    beforeEach(() => {
+        mockedExecute.mockClear();
+    });
+    it('should successfully create snyk dep graph from swift-pm dep tree', async () => {
 
-    const targetFile = path.join(__dirname, '../fixtures/Package.swift');
+        const targetFile = path.join(__dirname, '../fixtures/Package.swift');
+        const result = await computeDepGraph(path.join(__dirname, '../fixtures'), targetFile);
 
-    const result = await computeDepGraph(path.join(__dirname, '../fixtures'), targetFile);
+        expect(result).toMatchSnapshot();
+    });
 
-    expect(result).toMatchSnapshot();
-  });
+    it('should add additional parameters (one) to swiftpm cli', async () => {
+        const additionalArguments = ["firstParam"];
 
-  it('should add additional parameters to swiftpm cli', async () => {
-    const additionalArguments = ["doron"];
-    subprocessesMock["execute"] = jest.fn();
+        const targetFile = path.join(__dirname, '../fixtures/Package.swift');
+        await computeDepGraph(path.join(__dirname, '../fixtures'), targetFile, additionalArguments);
 
-    const mockedExecute = jest.mocked(execute);
-    mockedExecute.mockResolvedValueOnce(JSON.stringify(dependencies));
-    const targetFile = path.join(__dirname, '../fixtures/Package.swift');
-    await computeDepGraph(path.join(__dirname, '../fixtures'), targetFile, additionalArguments);
+        let swiftArguments: string[] = mockedExecute.mock.calls[0][1];
+        expect(swiftArguments[1]).toEqual(additionalArguments[0])
+    })
 
-    console.log(subprocessesMock["execute"].mock.calls);
+    it('should add additional parameters (many) to swiftpm cli', async () => {
+        const additionalArguments = ["firstParam", "secondParam", "thirdParam"];
 
-    // expect(execute).toHaveBeenCalledWith({})
-  })
+        const targetFile = path.join(__dirname, '../fixtures/Package.swift');
+        await computeDepGraph(path.join(__dirname, '../fixtures'), targetFile, additionalArguments);
+
+        let swiftArguments: string[] = mockedExecute.mock.calls[0][1];
+        expect(swiftArguments[1]).toEqual(additionalArguments[0])
+        expect(swiftArguments[2]).toEqual(additionalArguments[1])
+        expect(swiftArguments[3]).toEqual(additionalArguments[2])
+    })
+
+    it('should add additional parameters (none) to swiftpm cli', async () => {
+        const additionalArguments = undefined;
+
+        const targetFile = path.join(__dirname, '../fixtures/Package.swift');
+        await computeDepGraph(path.join(__dirname, '../fixtures'), targetFile, additionalArguments);
+
+        let swiftArguments = mockedExecute.mock.calls[0][1];
+        expect(swiftArguments.length).toEqual(SWIFT_DEFAULT_PARAMETERS_COUNT);
+    })
 });
