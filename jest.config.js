ca55083e3a7a8c756b8cdbe1a2a47c518d8f180c
Nicolas Séverin <nico-incubiq@users.noreply.github.com>
2026-06-30 14:58:50 +0100

    fix: [CMPA-552] set up trusted publishing (#45)
    * fix: [CMPA-552] set up trusted publishing
    
    * fix: [CMPA-552] dropped node 14 from test matrix as package requires node 16

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts)?$': 'ts-jest',
+    '^.+\\.(ts)?$': ['ts-jest', { tsconfig: 'test/tsconfig.json' }],
   },
   testMatch: ['**/*.spec.ts'],
   collectCoverage: false,
